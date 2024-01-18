b7073378661cca5d92801a91b978b33e9dda00f5
MrSASAMI <mrsasami@MrSASAMIdeMacBook-Pro.local>
2024-01-18 16:47:55 +0900

    normal

--- main.cc
+++ main.cc
@@ -1,26 +1,14 @@
 #include<iostream>
-#include"vec3.h"
-#include"ray.h"
+#include"rtweekend.h"
 #include"color.h"
+#include"hittable.h"
+#include"hittable_list.h"
+#include"sphere.h"
 
-double hit_sphere(const point3& center, double radius, const ray& r){
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
-    if(discriminant < 0){
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant)) / a;  
-    }
-}
-
-color ray_color(const ray& r){
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-    if(t > 0.0){
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+color ray_color(const ray& r, const hittable& world){
+    hit_record rec;
+    if(world.hit(r, 0, infinity, rec)){
+        return 0.5*(rec.normal + color(1,1,1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -32,9 +20,15 @@ int main(){
     //image
     auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
+
     //calculate the img height, and ensure that it's at least 1
     int image_height = static_cast<int>(image_width / aspect_ratio);
     image_height = (image_height < 1) ? 1 : image_height;
+
+    //world
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     //camera
     auto focal_length = 1.0;
@@ -64,7 +58,7 @@ int main(){
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
             
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
